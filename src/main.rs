dff90e314162c0523ff328d2c3c993e4d591024f
quvarxa <michaelchesser@outlook.com>
2015-02-26 19:39:08 +1030

    Update for changes in Bitmap library

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-#![feature(core, old_io, std_misc)]
+#![feature(core, fs, io, std_misc)]
 extern crate bitmap;
 extern crate noise;
 extern crate rand;
 
-use std::old_io::IoResult;
+use std::io;
+use std::fs::File;
+
 use bitmap::Bitmap;
 use wrapping2darray::Wrapping2DArray;
 
@@ -17,36 +19,37 @@ fn main() {
     let test = mapgen::UpperMap::new(SIZE, SIZE);
 
     println!("Saving elevation map");
-    let _ = elevation_bitmap(&test, "elevation.bmp");
+    elevation_bitmap(&test, "elevation.bmp").unwrap();
 
     println!("Saving flow map");
-    let _ = flow_bitmap(&test, "flow.bmp");
+    flow_bitmap(&test, "flow.bmp").unwrap();
 }
 
 /// Produce an elevation bitmap
-fn elevation_bitmap(map: &mapgen::UpperMap, filename: &str) -> IoResult<()> {
+fn elevation_bitmap(map: &mapgen::UpperMap, filename: &str) -> io::Result<()> {
     let land_colors = [color::consts::BEAVER, color::consts::BUFF];
     let sea_colors = [color::consts::AZURE, color::consts::COOL_BLACK];
 
-    let mut image = Bitmap::new(map.elevation.width() as i32, map.elevation.height() as i32);
+    let mut bitmap = Bitmap::new(map.elevation.width() as i32, map.elevation.height() as i32);
 
     for (idx, &h) in map.elevation.iter().enumerate() {
-        let x = idx as i32 % image.width();
-        let y = idx as i32 / image.width();
+        let x = idx as i32 % bitmap.width();
+        let y = idx as i32 / bitmap.width();
 
         // Map colors above (or equal to) 0.0 to land, and below 0.0 to sea
         if h >= 0.0 {
-            image.set_pixel(x, y, color::linear_gradient(&land_colors, h as f64).to_tuple());
+            bitmap.set_pixel(x, y, color::linear_gradient(&land_colors, h as f64).to_tuple());
         }
         else {
-            image.set_pixel(x, y, color::linear_gradient(&sea_colors, -h as f64).to_tuple());
+            bitmap.set_pixel(x, y, color::linear_gradient(&sea_colors, -h as f64).to_tuple());
         }
     }
-
-    image.write_to_file(filename)
+    
+    let mut file = try!(File::create(filename));
+    bitmap.write(&mut file)
 }
 /// Produce a flow bitmap
-fn flow_bitmap(map: &mapgen::UpperMap, filename: &str) -> IoResult<()> {
+fn flow_bitmap(map: &mapgen::UpperMap, filename: &str) -> io::Result<()> {
     let mut len_map = Wrapping2DArray::from_fn(map.ocean_flow.width(), map.ocean_flow.height(),
             |x, y| map.ocean_flow[(x, y)].length());
     mapgen::normalise(&mut len_map);
@@ -68,5 +71,6 @@ fn flow_bitmap(map: &mapgen::UpperMap, filename: &str) -> IoResult<()> {
         }
     }
 
-    bitmap.write_to_file(filename)
+    let mut file = try!(File::create(filename));
+    bitmap.write(&mut file)
 }
